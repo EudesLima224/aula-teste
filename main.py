c49dde1ee45d43f9810d457163cbbe8772eb77e6
EudesLima224 <eudeslimanovo224@gmail.com>
2025-04-04 18:33:18 -0300

    foi adicionado tiros
    agora o personagem atira, até  o momento sem bugs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pgzrun
-import pygame
+from pygame import Rect
+import time
 # Definir tamanho da tela
 WIDTH = 1200
 HEIGHT = 900
@@ -11,27 +12,52 @@ speed = 0
 indice_frame = 0
 animation = "idle"
 
+#medir fps
+frame_count = 0
+last_time = time.time()
+fps = 0
+
+#delay animações
+frame_counter = 0
+frame_delay = 8
 
 # Criar um Actor para a nuvem
-cloud1 = Actor("cloud1", (200, 100))  # Posição inicial (X=200, Y=100)
-cloud2 = Actor("cloud2", (650, 150))  # Posição inicial (X=200, Y=100)
+cloud1 = Actor("cloud1", (200, 100))
+cloud2 = Actor("cloud2", (650, 150))  
 #cloud3 = Actor("cloud3", )
 clouds = [cloud1, cloud2]
 
 #criação chão
 floor_height = HEIGHT - 20
-grassy_ground = Actor("grassy_ground", (0, floor_height))
 grassy_grounds = [Actor("grassy_ground", (x, floor_height)) for x in range(0, WIDTH + 64 + 64, 64)]  
 
 
 #criação do personagem
-character = Actor("character_idle1", (100, HEIGHT - 147))
+character_height = HEIGHT - 147
+animation_now = "character_idle0"
+character = Actor(animation_now, (300, floor_height - 128))
+speed_y = 0
+gravity = 0.8
+jump_strength = -18
+on_ground = True
+
+
 #animação do personagem
-walking_frames = [f"character_walk{n}" for n in range(7)]
+walking_frames = [f"character_walk{n}" for n in range(8)]
+character_idle = [f"character_idle{n}" for n in range(3)]
+character_gun = [f"character_gun{n}" for n in range(3)]
+shooting = False
+shot = 0
+bullets = []
+indice_idle_frame = 0
+indice_walking_frame = 0
+indice_gun_frame = 0
+character_jump = "character_jump"
 
 def draw():
     screen.clear()
     screen.fill((135,206,250))  # Cor de fundo azul(ceu)
+    screen.draw.text(f"FPS: {int(fps)}", (10, 10), fontsize = 30, color="white")
     #menu
     if not running:
         #nuvems do menu
@@ -43,31 +69,109 @@ def draw():
         #botão começar
         screen.draw.filled_rect(button_start, "blue")
         screen.draw.text("iniciar", (320, 240), fontsize=60, color="white")
+    global character, animation_now
     if running:
         for floor in grassy_grounds:
             floor.draw()  # Desenha cada pedaço do chão
+        character.image = animation_now
+        print(character.y)
         character.draw()
+        for bullet in bullets:
+            bullet.draw()
 
 
 def update():
-    global indice_frame, speed, animation
-    #verifica se ta andando
+    global last_time, frame_count, fps, grassy_grounds
+    frame_count += 1
+    current_time = time.time()
+    elapsed_time = current_time - last_time
+
+    #atualiza o fps a cada segundo
+    if elapsed_time >= 1:
+        fps = frame_count / elapsed_time
+        frame_count = 0
+        last_time = current_time
+    global indice_frame, speed, animation, frame_counter, character_height, floor_height, character_gun, indice_gun_frame
+    global speed_y, on_ground, jump_strength, shooting, shot
+
+
+    #verifica se ta andando-----------------------------
     if keyboard.right or keyboard.D:
         speed = 3
         animation = "walking"
-        #character.x += speed
-    else:
+    elif on_ground:
         speed = 0
         animation = "idle"
-    #altera os frames da animação3
+    #verifica o pulo
+    #elif character_height >= floor_height - 128:
+    if (keyboard.up or keyboard.W) and on_ground:
+        animation = "jump"
+        speed_y = jump_strength
+        on_ground = False
+    if keyboard.lctrl and shot == 0:
+        shooting = True
+
+    
+    #controle de frames animação
+    frame_counter += 1
+    
+    #animações
+    global indice_idle_frame, indice_walking_frame, animation_now, character
     if animation == "walking":
-        character.image = walking_frames[indice_frame % len(walking_frames)]
-    #elif animation == "idle":
-     #   character.image = 
-    indice_frame = (indice_frame + 1) % len(walking_frames)
+        if frame_counter >= 8:
+            frame_counter = 0
+            if on_ground:
+                animation_now = walking_frames[indice_walking_frame % len(walking_frames)]
+                print(animation_now)
+                indice_walking_frame = (indice_walking_frame + 1) % len(walking_frames)
+    
+
+    elif animation == "jump":
+            animation_now = character_jump
+            #print(character.y)
+
+    #gravidade
+    speed_y += gravity
+    character.y += speed_y
+    for floor in grassy_grounds:
+        if character.colliderect(floor):
+            character.y = floor.top - character.height / 2
+            speed_y = 0
+            on_ground = True
+            break  # Já colidiu com um, não precisa verificar os outros
 
 
-    global grassy_grounds
+    if animation == "idle":
+        speed = 0
+        if frame_counter >= 20:
+            frame_counter = 0
+            animation_now = character_idle[indice_idle_frame % len(character_idle)]
+            print(animation_now)
+            if indice_idle_frame <= len(character_idle):
+                indice_idle_frame = indice_idle_frame + 1
+            elif indice_idle_frame > len(character_idle):
+                indice_idle_frame = 0
+    for bullet in bullets[:]:
+        bullet.x += 10
+        if bullet.x > WIDTH + 50:
+            bullets.remove(bullet)
+    if shooting and shot == 0:
+        if frame_counter >= 1:
+            frame_counter = 0
+            animation_now = character_gun[indice_gun_frame % len(character_gun)]
+            print(animation_now)
+            indice_gun_frame = (indice_gun_frame + 1) % len(character_gun)
+            if animation_now == character_gun[2]:
+                shooting = False
+                shot = 60
+                # Cria o tiro baseado na posição atual do personagem
+                bullet = Actor("bullet", (character.x + 80, character.y + 50))
+                bullets.append(bullet)
+
+    if shot > 0:
+        shot -= 1
+
+
     #move o chão para a esquerda
     for floor in grassy_grounds:
         floor.x -= speed
--- teste.py
+++ teste.py
@@ -1,28 +1,143 @@
 import pgzrun
 import pygame
+import time
+
 # Definir tamanho da tela
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 1200
+HEIGHT = 900
 
+# Variável de controle
+running = False
+button_start = Rect((WIDTH // 2 - 150), 220, 300, 80)
+speed = 0
+indice_frame = 0
+animation = "idle"
 
+# Medir FPS
+frame_count = 0
+last_time = time.time()
+fps = 0
 
+# Delay animações
+frame_counter = 0
+frame_delay = 8
 
-cloud1 = Actor("cloud1", (200, 100))  # Posição inicial (X=200, Y=100)
-cloud2 = Actor("cloud2", (650, 100))  # Posição inicial (X=200, Y=100)
+# Criar nuvens
+cloud1 = Actor("cloud1", (200, 100))
+cloud2 = Actor("cloud2", (650, 150))
 clouds = [cloud1, cloud2]
+
+# Criar chão
+floor_height = HEIGHT - 20
+grassy_grounds = [Actor("grassy_ground", (x, floor_height)) for x in range(0, WIDTH + 64 + 64, 64)]  
+
+# Criar personagem
+character_height = HEIGHT - 147
+animation_now = "character_idle0"
+character = Actor(animation_now, (300, character_height))
+
+# Variáveis do pulo e gravidade
+speed_y = 0
+gravity = 0.5
+jump_strength = -10
+on_ground = True
+
+# Animações do personagem
+walking_frames = [f"character_walk{n}" for n in range(8)]
+character_idle = [f"character_idle{n}" for n in range(3)]
+indice_idle_frame = 0
+indice_walking_frame = 0
+character_jump = "character_jump"
+
 def draw():
     screen.clear()
-    screen.fill((135,206,250))  # Cor de fundo azul(ceu)
+    screen.fill((135, 206, 250))  # Cor de fundo azul (céu)
+    screen.draw.text(f"FPS: {int(fps)}", (10, 10), fontsize=30, color="white")
 
-    for cloud in clouds:
-        cloud.draw()
+    # Menu inicial
+    if not running:
+        for cloud in clouds:
+            cloud.draw()
+        screen.draw.filled_rect(Rect((WIDTH // 2 - 150), 45, 300, 80), "blue")
+        screen.draw.text("Meu Jogo", (270, 60), fontsize=80, color="white")
+        screen.draw.filled_rect(button_start, "blue")
+        screen.draw.text("iniciar", (320, 240), fontsize=60, color="white")
+    else:
+        for floor in grassy_grounds:
+            floor.draw()  
+        character.y = character_height  # Atualiza a posição do personagem
+        character.draw()
 
 
+def update():
+    global last_time, frame_count, fps
+    frame_count += 1
+    current_time = time.time()
+    elapsed_time = current_time - last_time
+
+    # Atualiza o FPS a cada segundo
+    if elapsed_time >= 1:
+        fps = frame_count / elapsed_time
+        frame_count = 0
+        last_time = current_time
+
+    global indice_frame, speed, animation, frame_counter, character_height
+    global speed_y, on_ground, jump_strength
+
+    # Movimento lateral
+    if keyboard.right or keyboard.D:
+        speed = 3
+        animation = "walking"
+
+    # Pulo
+    if (keyboard.up or keyboard.W) and on_ground:
+        animation = "jump"
+        speed_y = jump_strength
+        on_ground = False
+
+    # Gravidade
+    speed_y += gravity  # Aplica a gravidade
+    character_height += speed_y  # Atualiza a altura do personagem
+
+    # Verifica colisão com o chão
+    if character_height >= HEIGHT - 147:
+        character_height = HEIGHT - 147
+        speed_y = 0
+        on_ground = True
+
+    # Controle de frames da animação
+    frame_counter += 1
+    global indice_idle_frame, indice_walking_frame, animation_now
+
+    if animation == "walking":
+        if frame_counter >= 8:
+            frame_counter = 0
+            animation_now = walking_frames[indice_walking_frame % len(walking_frames)]
+            indice_walking_frame = (indice_walking_frame + 1) % len(walking_frames)
+
+    elif animation == "jump":
+        animation_now = character_jump
+
+    if animation == "idle":
+        speed = 0
+        if frame_counter >= 20:
+            frame_counter = 0
+            animation_now = character_idle[indice_idle_frame % len(character_idle)]
+            indice_idle_frame = (indice_idle_frame + 1) % len(character_idle)
+
+    # Movimentação do chão
+    for floor in grassy_grounds:
+        floor.x -= speed
+    if grassy_grounds[0].x < -64:
+        grassy_grounds.pop(0)
+        new_grassy_ground = Actor("grassy_ground", (grassy_grounds[-1].x + 64, floor_height))
+        grassy_grounds.append(new_grassy_ground)
 
 
-
-
-
+def on_mouse_down(pos):
+    global running
+    if not running and button_start.collidepoint(pos):
+        running = True
 
 
 # Iniciar o jogo
