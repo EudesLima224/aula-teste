65ccd9d175362260c425c84f11da6f9d80d1c206
EudesLima224 <eudeslimanovo224@gmail.com>
2025-04-07 14:28:53 -0300

    exclusão de arquivos inuteis

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import random
 WIDTH = 1200
 HEIGHT = 900
 FLOOR_HEIGHT = HEIGHT - 20
-CHARACTER_Y = FLOOR_HEIGHT - 128
 CLOUD_POSITIONS = [(200, 100), (650, 150)]
 FRAME_DELAY = 8
 music_on = True
@@ -17,7 +16,6 @@ sound_game_over = True
 # === VARIÁVEIS GLOBAIS ===
 running = False
 player_direction = 0  # -1 esquerda, 1 direita, 0 parado
-world_speed = 0       # velocidade do mundo (chão, inimigos etc)
 speed_y = 0
 gravity = 0.8
 jump_strength = -18
@@ -26,11 +24,15 @@ frame_counter = 0
 shot_cooldown = 0
 shooting = False
 enemy_spawn_timer = 0
+enemy_spawn_delay = 150
+enemy_bullets = []
+
 
 # === INTERFACE ===
 button_start = Rect((WIDTH // 2 - 200), 400, 400, 80)
 button_music = Rect((WIDTH // 2 - 200), 500, 180, 80)
 button_sound = Rect((WIDTH // 2 + 20), 500, 180, 80)
+button_quit = Rect((WIDTH// 2 - 200), 600, 400, 80)
 
 # === CENÁRIO ===
 clouds = [Actor(f"cloud{i+1}", pos) for i, pos in enumerate(CLOUD_POSITIONS)]
@@ -39,8 +41,7 @@ grassy_grounds = [Actor("grassy_ground", (x, FLOOR_HEIGHT)) for x in range(0, WI
 
 class Character:
     def __init__(self):
-        global CHARACTER_Y
-        self.actor = Actor("character_idle0", (250, CHARACTER_Y))
+        self.actor = Actor("character_idle0", (250, FLOOR_HEIGHT - 128))
         self.animation_state = "idle"
         self.indice_idle = 0
         self.indice_walk = 0
@@ -50,7 +51,7 @@ class Character:
         self.walk_left = [f"character_walk_left{i}" for i in range(8)]
         self.gun = [f"character_gun{i}" for i in range(3)]
         self.jump = "character_jump"
-        self.location = {"x": 150, "y": CHARACTER_Y}
+        self.life = 5
     def draw(self):
         self.actor.draw()
 
@@ -61,7 +62,6 @@ player = Character()
 bullets = []
 icon_bullets = 4
 bullets_magazine = []
-bulletsicon = Actor("life")
 bulletsicon_position = {"x": 20, "y": 60}
 for icon in range(icon_bullets):
     bullets_magazine.append(Actor("bulleticon", (bulletsicon_position["x"], bulletsicon_position["y"])))
@@ -70,23 +70,60 @@ spawn_bullet = 0
 
 # ===vida===
 lifes_character = []
-life = Actor("life")
-life_character = 5
 life_position = {"x": 20, "y": 20}
-for life in range(life_character):
+for life in range(player.life):
     lifes_character.append(Actor("life", (life_position["x"], life_position["y"])))
     life_position["x"] += 30
 CHARACTER_LIMIT_RIGHT = 300
 CHARACTER_LIMIT_LEFT = 20
 
+class Enemy:
+    def __init__(self, x, y):
+        self.actor = Actor("enemy_robot_walk0", (x, y))
+        self.walk_frames = [f"enemy_robot_walk{i}" for i in range(8)]
+        self.attack_frames = [f"enemy_robot_attack{i}" for i in range(2)]
+        self.frame_index = 0
+        self.attacking = False
+        self.attack_timer = 0
+        self.shot = False
+        self.life = 3
+        self.direction = -1
+        self.speed = 1.8
+
+    def update(self):
+        dist = abs(self.actor.x - player.actor.x)
+        if dist <= 750:
+            self.attacking = True
+            self.attack_timer += 1
+            if self.attack_timer < 30:
+                self.actor.image = self.attack_frames[0]
+            elif self.attack_timer == 30:
+                self.actor.image = self.attack_frames[1]
+                if not self.shot:
+                    bullet = Actor("bullet", (self.actor.x - 40, self.actor.y + 50))
+                    bullet.direction = -1
+                    enemy_bullets.append(bullet)
+                    self.shot = True
+                    if sound_on:
+                        sounds.lasersmall_003.play()
+            elif self.attack_timer > 60:
+                self.attack_timer = 0
+                self.shot = False
+        else:
+            self.attacking = False
+            self.attack_timer = 0
+            self.shot = False
+            self.actor.x += self.direction * self.speed
+            if frame_counter % 8 == 0:
+                self.frame_index = (self.frame_index + 1) % len(self.walk_frames)
+                self.actor.image = self.walk_frames[self.frame_index]
+
+    def draw(self):
+        self.actor.draw()
+
 
 # === INIMIGOS ===
 enemies = []
-enemy_frames = [f"enemy_robot_walk{i}" for i in range(8)]
-enemy_attack_frames = [f"enemy_robot_attack{i}" for i in range(2)]
-enemy_bullets = []
-life_enemy = 3
-enemy_spawn_delay = 50
 
 # === FUNÇÕES PRINCIPAIS ===
 def draw():
@@ -105,31 +142,28 @@ def update():
     update_player_input()
     update_character()
     update_bullets()
-    update_enemy_bullets()  # <- Adicionado aqui
+    update_enemy_bullets()
     update_enemies()
     update_floor()
 
 
-def on_mouse_down(pos):
+def on_mouse_down(pos, button):
     global running, music_on, sound_on, shooting, shot_cooldown
-    if not running and button_start.collidepoint(pos):
-        running = True
-    if not running and button_music.collidepoint(pos):
-        music_on = not music_on
-        if music_on:
-            print("Música ligada")
-            start_music()
-        else:
-            print("Música desligada")
-            stop_music()
-    if not running and button_sound.collidepoint(pos):
-        sound_on = not sound_on
-        if sound_on:
-            print("som ligado")
-        else:
-            print("som desligado")
-    if running and shot_cooldown == 0 and len(bullets_magazine) > 0:
-        shooting = True
+    if button == mouse.LEFT:
+        if not running and button_start.collidepoint(pos):
+            running = True
+        if not running and button_music.collidepoint(pos):
+            music_on = not music_on
+            if music_on:
+                start_music()
+            else:
+                stop_music()
+        if not running and button_sound.collidepoint(pos):
+            sound_on = not sound_on
+        if running and shot_cooldown == 0 and len(bullets_magazine) > 0:
+            shooting = True
+        if button_quit.collidepoint(pos) and not running:
+            quit()
 
 
 # === DESENHO ===
@@ -140,7 +174,9 @@ def draw_menu():
     screen.draw.filled_rect(Rect((WIDTH // 2 - 250), 45, 500, 80), "blue")
     screen.draw.text("Robotic Adventures", ((WIDTH // 2 - 200), 65), fontsize=60, color="white")
     screen.draw.filled_rect(button_start, "blue")
-    screen.draw.text("iniciar", ((WIDTH // 2 - 65), 420), fontsize=60, color="white")
+    screen.draw.text("Start", ((WIDTH // 2 - 55), 420), fontsize=60, color="white")
+    screen.draw.filled_rect(button_quit, "blue")
+    screen.draw.text("Quit", ((WIDTH // 2 - 55), 620), fontsize=60, color="white")
     #botão musica
     if music_on:
         screen.draw.filled_rect(button_music, "blue")
@@ -155,27 +191,11 @@ def draw_menu():
 
 def draw_gameplay():
     global frame_counter, game_over_time, sound_game_over, running
-    for floor in grassy_grounds:
-        floor.draw()
-
+    for group in (grassy_grounds, bullets, enemies, enemy_bullets, lifes_character, bullets_magazine):
+        for intem in group:
+            intem.draw()
     player.draw()
-
-    for bullet in bullets:
-        bullet.draw()
-
-
-    for enemy in enemies:
-        enemy["actor"].draw()
-
-    for bullet in enemy_bullets:
-        bullet.draw()
-    
-    for life in lifes_character:
-        life.draw()
-    for bulletincon in bullets_magazine:
-        bulletincon.draw()
-
-    if life_character <=0:
+    if player.life <=0:
         screen.draw.filled_rect(Rect((WIDTH // 2 - 250), 520, 500, 100), "blue")
         screen.draw.text("GAME OVER!",((WIDTH // 2 - 200), 540), fontsize=90, color='white')
         game_over_time += 1
@@ -191,26 +211,17 @@ def draw_gameplay():
 
 
 def reset_game():
-    global enemies, bullets, enemy_bullets, speed, speed_y, on_ground, life_character
+    global enemies, bullets, enemy_bullets, speed_y, on_ground, player
     enemies = []
     bullets = []
     enemy_bullets = []
-    speed = 0
     speed_y = 0
     on_ground = True
-    player.location["x"] = 300
-    player.location["y"] = CHARACTER_Y
-    life_character = 5
+    player = Character()
     life_position = {"x": 20, "y": 20}
-    for life in range(life_character):
+    for life in range(player.life):
         lifes_character.append(Actor("life", (life_position["x"], life_position["y"])))
         life_position["x"] += 30
-
-    # Resetar chão
-    grassy_grounds.clear()
-    for x in range(0, WIDTH + 128, 64):
-        grassy_grounds.append(Actor("grassy_ground", (x, FLOOR_HEIGHT)))
-
 
 
 # === CONTROLE DO JOGADOR ===
@@ -228,22 +239,18 @@ def update_player_input():
         player.animation_state = "walking_left"
     elif on_ground:
         player.animation_state = "idle"
-
     if (keys.up or keys.w) and on_ground:
         on_ground = False
         player.animation_state = "jump"
         speed_y = jump_strength
         if sound_on:
             sounds.jump1.play()
-
-
     if keys.lctrl and shot_cooldown == 0 and len(bullets_magazine) > 0:
             shooting = True
 
 # === ANIMAÇÃO DO PERSONAGEM ===
 def update_character():
-    global frame_counter, character, speed_y, on_ground
-    global indice_idle, indice_walk, indice_gun, shot_cooldown, shooting
+    global shot_cooldown, shooting, frame_counter, speed_y, on_ground
 
     frame_counter += 1
 
@@ -269,14 +276,14 @@ def update_character():
     # Animação de tiro
     if shooting and shot_cooldown == 0:
         if frame_counter >= 1:
-            player.image = player.gun[player.indice_gun % len(player.gun)]
+            player.actor.image = player.gun[player.indice_gun % len(player.gun)]
             player.indice_gun += 1
             frame_counter = 0
             if player.indice_gun >= len(player.gun):
-                indice_gun = 0
+                player.indice_gun = 0
                 shooting = False
                 shot_cooldown = 20
-                bullets.append(Actor("bullet", (player.location["x"] + 80, player.location["y"] + 50)))
+                bullets.append(Actor("bullet", (player.actor.x + 60, player.actor.y + 50)))
                 bullets_magazine.pop(-1)
                 if sound_on:
                     sounds.laserretro_004.play()
@@ -286,30 +293,23 @@ def update_character():
 
     # Gravidade
     speed_y += gravity
-    player.location["y"] += speed_y
-    player.actor.y = player.location["y"]
-    player.actor.x = player.location["x"]
-
-
+    player.actor.y += speed_y
     for floor in grassy_grounds:
         if player.actor.colliderect(floor):
-            player.location["y"] = floor.top - player.actor.height / 2
+            player.actor.y = floor.top - player.actor.height / 2
             speed_y = 0
             on_ground = True
             break
 
     # Movimento horizontal com limites de tela
     if player_direction != 0:
-        if player_direction == -1 and player.location["x"] > CHARACTER_LIMIT_LEFT:
-            player.location["x"] += player_direction * 3
-        elif player_direction == 1 and player.location["x"] < CHARACTER_LIMIT_RIGHT:
-            player.location["x"] += player_direction * 3
+        if player_direction == -1 and player.actor.x > CHARACTER_LIMIT_LEFT:
+            player.actor.x += player_direction * 3
+        elif player_direction == 1 and player.actor.x < CHARACTER_LIMIT_RIGHT:
+            player.actor.x += player_direction * 3
         else:
             if player_direction == 1:
                 move_world(-3)
-
-
-
 
 
 # === TIROS DO JOGADOR ===
@@ -326,9 +326,9 @@ def update_bullets():
             bullets.remove(bullet)
         else:
             for enemy in enemies[:]:
-                if bullet.colliderect(enemy["actor"]):
-                    enemy["life"] -= 1
-                    if enemy["life"] <= 0:
+                if bullet.colliderect(enemy.actor):
+                    enemy.life -= 1
+                    if enemy.life <= 0:
                         enemies.remove(enemy)
                     if bullet in bullets:
                         bullets.remove(bullet)
@@ -337,102 +337,53 @@ def update_bullets():
 # === INIMIGOS ===
 def update_enemies():
     global enemy_spawn_timer, enemy_spawn_delay
+
     enemy_spawn_timer += 1
 
     if enemy_spawn_timer >= enemy_spawn_delay:
-        print(f"{enemy_spawn_delay}")
         spawn_enemy()
         enemy_spawn_timer = 0
         enemy_spawn_delay = random.randint(100, 340)
 
     for enemy in enemies[:]:
-        update_enemy(enemy)
-        if enemy["actor"].x <= -50:
+        enemy.update()
+        if enemy.actor.x <= -50:
             enemies.remove(enemy)
+
 
 def spawn_enemy():
     if sound_on:
         sounds.powerup12.play()
-    enemy = {
-        "actor": Actor(enemy_frames[0], (WIDTH + 50, FLOOR_HEIGHT - 128)),
-        "frame_index": 0,
-        "attacking": False,
-        "attack_timer": 0,
-        "shot": False,
-        "life": 3
-    }
+    enemy = Enemy(WIDTH + 50, FLOOR_HEIGHT - 128)
     enemies.append(enemy)
 
-def update_enemy(enemy):
-    dist = abs(enemy["actor"].x - player.location["x"])
-    if dist <= 750:
-        enemy["attacking"] = True
-        enemy["attack_timer"] += 1
-
-        if enemy["attack_timer"] < 30:
-            enemy["actor"].image = enemy_attack_frames[0]
-        elif enemy["attack_timer"] == 30:
-            enemy["actor"].image = enemy_attack_frames[1]
-            if not enemy["shot"]:
-                bullet = Actor("bullet", (enemy["actor"].x - 40, enemy["actor"].y + 50))
-                bullet.direction = -1
-                enemy_bullets.append(bullet)
-                enemy["shot"] = True
-                if sound_on:
-                    sounds.lasersmall_003.play()
-        elif enemy["attack_timer"] > 60:
-            enemy["attack_timer"] = 0
-            enemy["shot"] = False
-    else:
-        enemy["actor"].x -= 1.8
-        enemy["attacking"] = False
-        enemy["attack_timer"] = 0
-        enemy["shot"] = False
-
-        if frame_counter % 8 == 0:
-            enemy["frame_index"] = (enemy["frame_index"] + 1) % len(enemy_frames)
-            enemy["actor"].image = enemy_frames[enemy["frame_index"]]
 
 # === CHÃO ===
 def update_floor():
-    global world_speed
-    for floor in grassy_grounds:
-        floor.x -= world_speed
-
     if grassy_grounds[0].x < -64:
         grassy_grounds.pop(0)
         new_floor = Actor("grassy_ground", (grassy_grounds[-1].x + 64, FLOOR_HEIGHT))
         grassy_grounds.append(new_floor)
 
 def update_enemy_bullets():
-    global life_character
     for bullet in enemy_bullets[:]:
-        bullet.x += bullet.direction * 10
-        if bullet.right < 0 or bullet.left > WIDTH:
+        bullet.x += bullet.direction * 6  #
+        if bullet.x < -50 or bullet.x > WIDTH + 50:
             enemy_bullets.remove(bullet)
         elif bullet.colliderect(player.actor):
-            life_character -= 1
-            if life_character >= 0:
-                lifes_character.pop(-1)
-            print(lifes_character)
-            print("Personagem atingido!", life_character)
             enemy_bullets.remove(bullet)
+            player.life -= 1
+            if len(lifes_character) > 0:
+                lifes_character.pop(-1)
+
 
 def move_world(offset):
-    # Move o chão
-    for floor in grassy_grounds:
-        floor.x += offset
-
+    for group in (grassy_grounds, bullets, enemy_bullets):
+        for item in group:
+            item. x += offset
     # Move os inimigos
     for enemy in enemies:
-        enemy["actor"].x += offset
-
-    # Move as balas
-    for bullet in bullets:
-        bullet.x += offset
-
-    for bullet in enemy_bullets:
-        bullet.x += offset
+        enemy.actor.x += offset
 
 # === MUSICA ===
 def start_music():
@@ -445,8 +396,6 @@ def stop_music():
     if music.is_playing("music"):
         music.stop()
 
-#def sons(som):
-    
 
 # === INICIAR O JOGO ===
 pgzrun.go()
